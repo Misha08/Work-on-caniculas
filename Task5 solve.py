2d16a1fb388facc932bc9022528d11671cc9d5f6
Mihail <viktor.svorikin@gmail.com>
2021-10-27 23:57:15 +0200

    Last solving

--- Task5 solve.py
+++ Task5 solve.py
@@ -1,2 +1,19 @@
 a, b, c = float(input()), float(input()), float(input())
 x, y = float(input()), float(input())
+
+yes_string = 'Кирпич влазеет в дырку'
+
+if a <= x and b <= y:
+    print(yes_string)
+elif a <= y and b <= x:
+    print(yes_string)
+elif b <= x and c <= y:
+    print(yes_string)
+elif c <= x and b <= y:
+    print(yes_string)
+elif a <= x and c <= y:
+    print(yes_string)
+elif c <= x and a <= y:
+    print(yes_string)
+else:
+    print('Кирпич не влазеет в дырку')
